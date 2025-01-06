064d5af200cb805e4d0ae79150078e18a269f719
cafuentes <cfuentesmedina@gmail.com>
2025-01-06 18:04:22 -0300

    feat: Handlebars Home view created

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -2,7 +2,17 @@ import { NotFoundError } from "../errors/TypeError.js";
 import { Producto } from "../models/Producto.model.js"
 import { isEmptyResponseData } from "../utils/validations/Validate.js";
 
-
+// API RESTFULL
+// REST -> ARQUITECTURA DE DISEÑO PARA SERVICIOS WEB - ROY FIELDING
+// REPRESENT STATE TRANSFER
+/* 
+1. separa responsbilidad  -> cliente y servidor -> cliente solicita - servidor provee o almacena
+2. stateless (sin estado) -> cada peticions del cliente al servidor debe ser STATELESS (no habrá cambios en el estado hasta que el proceso termine)
+3. interfaz uniforme -> las respuestas deben contener una estructura homogenea, al igual que los endpoints para cada petición
+4. cacheable -> las respuestas y las peticiones deben ser almacenadas en cache (espacio de memoria temporal) para mejorar rendimiento y escalabilidad
+5. uso representacional, el cliente y el servidor se comunican a través de representaciones de recursos (JSON, XML, HTML)
+6. sistema en capas -> el cliente no necesita saber la estructura de la base de datos, solo necesita saber la estructura de los recursos(balanceadores de carga, servidores, proxies, gateways)
+*/
 
 export const createProduct = async (req, res, next) => {
     try {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
+import path from 'path';
+
 import { serverInit } from './services/serverInit.js';
 
 
@@ -8,12 +11,32 @@ import { errorHandler } from './middlewares/errorHandlers.js';
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.static(path.join(process.cwd(), 'public')));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine('hbs', engine({ 
+    extname: '.hbs',
+    layoutsDir: path.join(process.cwd(), 'src','views', 'layouts'),
+    defaultLayout: 'main',
+    partialsDir: path.join(process.cwd(), 'src', 'views', 'partials')
+}));
+
+
+app.set('views', path.join(process.cwd(), 'src','views'));
+app.set('view engine', '.hbs');
+
+// cwd: current working directory
+
 app.use('/api/v1', router);
+
 app.use(errorHandler);
+
+app.get('/', (req, res) => {
+    res.render('pages/home');
+})
 
 
 serverInit(app, PORT)
